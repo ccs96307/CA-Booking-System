865fb046bdbf5dffb16aa71124b1b6fe44ee3a03
ccs96307 <ccs96307@gmail.com>
2021-12-16 15:12:39 +0800

    0.0.2

--- reserve-system.php
+++ reserve-system.php
@@ -98,12 +98,23 @@ function reserve_system_setting_page() {
         <table class="form-table">
             <p>
                 <?php 
-                    echo json_encode(get_option( 'event_name' ));
-                    echo json_encode(get_option( 'ca_reserve_list' ));
+                    $users = get_users( array( 'fields' => array( 'ID' ) ) );
 
-                    global $new_whitelist_options;
-                    $option_names = $new_whitelist_options[ 'tnt-settings-group' ];
-                    echo json_encode( $option_names );
+                    foreach( $users as $user ) {
+                        $capabilities = get_user_meta( $user->ID )['wp_capabilities'];
+                        if (preg_match( '/um_teacher/', implode(',', $capabilities) )) {
+                            //echo json_encode(get_user_meta( $user->ID )['wp_capabilities']);
+                        }
+                    }
+
+                    //echo json_encode(get_option( 'event_name' ));
+                    //echo json_encode(get_option( 'ca_reserve_list' ));
+                    //global $new_whitelist_options;
+                    //$option_names = $new_whitelist_options[ 'tnt-settings-group' ];
+                    //echo json_encode( $option_names );
+                    
+                    //echo file_get_contents('./html/reserve_page.html');
+                    echo dirname( __DIR__ . '/html/reserve_page.html' );
                 ?>
             </p>
         </table>
@@ -113,6 +124,8 @@ function reserve_system_setting_page() {
 
 <?php 
 }
+
+
 
 
 
@@ -126,240 +139,7 @@ function reserve_system_page_init( $content ) {
         $all_users = get_users( array( 'role__in' => array( 'author', '' )) );
 
         // Content
-        $content = "
-<html lang='en'>
-<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
-<head>
-    <script>
-        function Search() {
-            $.ajax({
-                type: 'POST',
-                url: 'https://allthing-can.com/test.php',
-                data: {
-                    'type': 'search',
-                    'id': $('#search_id').val(),
-                    '_mon': $('#current_mon').text(),
-                    '_tue': $('#current_tue').text()
-                },
-                dataType: 'json',
-                success: function (str) {
-                    document.getElementById('result').innerHTML = str.name;
-                },
-                error: function (e) {
-                    alert('something error');
-                },
-                beforeSend: function() {
-                    
-                }
-            });
-        }
-    </script>
-</head>
-
-<style>
-table, tr, td {
-    border: 1px solid black;
-    text-align: center;
-}
-
-caption {
-    text-align: center;
-}
-
-</style>
-
-
-<body>
-    <!-- Weekly calendar --!>
-    <table>
-        <caption id='current_month'><h3></h3></caption>
-        <tr>
-            <td>Mon</td>
-            <td>Tue</td>
-            <td>Wed</td>
-            <td>Thu</td>
-            <td>Fri</td>
-            <td style='color: red;'>Sat</td>
-            <td style='color: red;'>Sun</td>
-        </tr>
-        <tr>
-            <td id='current_mon'></td>
-            <td id='current_tue'></td>
-            <td id='current_wed'></td>
-            <td id='current_thu'></td>
-            <td id='current_fri'></td>
-            <td id='current_sat'></td>
-            <td id='current_sun'></td>
-        </tr>    
-    </table>
-
-    <button align='left' type='button' onclick='prev()'>PREV</button>
-    <button align='right' type='button' onclick='next()'>NEXT</button>
-    <br>
-
-    My name is <span id='result'></span>.<hr />
-    <input id='search_id' />
-<button onclick='Search()'>Search</button>
-
-<p>get_option('event'): " . get_option( 'event_name' ) . "</p>
-<p>Username: $current_user->user_login</p>
-<p>User email: $current_user->email</p>
-<p>User first name: $current_user->firstname</p>
-<p>User last name: $current_user->lastname</p>
-<p>User display name: $current_user->display_name</p>
-<p>User ID: $current_user->ID</p>
-</body>
-</html>
-
-<script>
-    const _months = [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-        'July',
-        'August',
-        'September',
-        'October',
-        'November',
-        'December'
-    ];
-
-    const _weekdays = [
-        'Sunday',
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday',
-    ];
-
-    // Init
-    var _date = new Date();
-    _date.setDate(_date.getDate()-_date.getDay()+1);
-
-    // Format method
-    function formatDate(format_date) {
-        let month = (1 + format_date.getMonth()).toString().padStart(2, '0');
-        let day = format_date.getDate().toString().padStart(2, '0');
-
-        return month + '/' + day;
-    }
-
-    // Set date function
-    function setWeekDate(direction) {
-        // Direction
-        if (direction == -1) {
-            _date.setDate(_date.getDate()-14);
-        }
-
-        // Months and Years
-        const months = [];
-        const years = [];
-        
-        // Current weekday
-        months[0] = _date.getMonth();
-        years[0] = _date.getFullYear();
-
-        document.getElementById('current_mon').innerHTML = formatDate(_date);
-        _date.setDate(_date.getDate()+1);
-
-        if (months[0] != _date.getMonth()) {
-            months[1] = _date.getMonth();
-        }
-        if (years[0] != _date.getFullYear()) {
-            months[1] = _date.getFullYear();
-        }
-
-        document.getElementById('current_tue').innerHTML = formatDate(_date);
-        _date.setDate(_date.getDate()+1);
-
-        if (months[0] != _date.getMonth()) {
-            months[1] = _date.getMonth();
-        }
-        if (years[0] != _date.getFullYear()) {
-            months[1] = _date.getFullYear();
-        }
-
-        document.getElementById('current_wed').innerHTML = formatDate(_date);
-        _date.setDate(_date.getDate()+1);
- 
-        if (months[0] != _date.getMonth()) {
-            months[1] = _date.getMonth();
-        }
-        if (years[0] != _date.getFullYear()) {
-            months[1] = _date.getFullYear();
-        }
-       
-        document.getElementById('current_thu').innerHTML = formatDate(_date);
-        _date.setDate(_date.getDate()+1);
- 
-        if (months[0] != _date.getMonth()) {
-            months[1] = _date.getMonth();
-        }
-        if (years[0] != _date.getFullYear()) {
-            months[1] = _date.getFullYear();
-        }
-       
-        document.getElementById('current_fri').innerHTML = formatDate(_date);
-        _date.setDate(_date.getDate()+1);
- 
-        if (months[0] != _date.getMonth()) {
-            months[1] = _date.getMonth();
-        }
-        if (years[0] != _date.getFullYear()) {
-            months[1] = _date.getFullYear();
-        }
-       
-        document.getElementById('current_sat').innerHTML = formatDate(_date);
-        _date.setDate(_date.getDate()+1);
-
-        if (months[0] != _date.getMonth()) {
-            months[1] = _date.getMonth();
-        }
-        if (years[0] != _date.getFullYear()) {
-            months[1] = _date.getFullYear();
-        }
-
-        document.getElementById('current_sun').innerHTML = formatDate(_date);
-        _date.setDate(_date.getDate()+1);
-
-        // Current Month and Year
-        if (months.length == 2 && years.length == 2) {
-            document.getElementById('current_month').innerHTML = _months[months[0]] + ' ' + years[0] + ' - ' + _months[months[1]] + years[1];
-        }
-        else if (months.lenth == 2) {
-             document.getElementById('current_month').innerHTML = _months[months[0]] + ' ' + years[0] + ' - ' + _months[months[1]] + years[0];         
-        }
-        else {
-            document.getElementById('current_month').innerHTML = _months[_date.getMonth()] + ' ' + _date.getFullYear();
-        }
-    }
-
-    function next() {
-        setWeekDate(1);
-        Search();
-    }
-
-    function prev() {
-        setWeekDate(-1);
-        Search();
-    }
-
-    
-    // Load the script
-    window.onload = function() {
-        setWeekDate(1);
-    };
-
-</script>
-
-
-
-";
+        $content = file_get_contents( __DIR__ . '/html/reserve_page.html' );
     }
 
     return $content;
--- test.php
+++ test.php
@@ -2,29 +2,27 @@
 require_once('wp-load.php');
 
 
+// Init
 $type = $_POST["type"];
 $id = $_POST["id"];
 $_mon = $_POST["_mon"];
 $_tue = $_POST["_tue"];
 
-$result = array();
+// Return results
 $results = array();
 
+
+// Search event
 if ($type == "search") {
     if ( $id != NULL) {
         update_option( 'event_name', $id );
         $name = $id; 
     }
+
+    $results = array("name" => $_mon);
 }
 
+echo json_encode($results);
 
-$result = array("name" => $_mon);
-$results = array(
-    "mon" => $_mon,
-    "tue" => $_tue
-);
-
-echo json_encode($result);
-// echo json_encode($results);
 
 ?>
